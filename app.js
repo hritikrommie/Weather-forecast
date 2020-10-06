79ba12d2c6bafa594f40576b0743e6f91e6cefe9
HRITIK DWIVEDI <44141619+hritikrommie@users.noreply.github.com>
2020-10-06 23:50:34 +0530

    Adding icons

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const form = document.querySelector('.form-control');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const city = form.city.value.trim();
-    Send(city,Receive);
+    Send(city, Receive);
     form.reset();
 });
 
@@ -18,7 +18,7 @@ function Send(city, callback) {
         const data = JSON.parse(request.responseText);
         const loc_key = data[0].Key;
         console.log(loc_key);
-        callback(loc_key,city,populate);
+        callback(loc_key, city, populate);
     };
     request.send();
 }
@@ -30,18 +30,27 @@ function Receive(loc_key,city,callback) {
     frcast.open('GET', forecast + fetch_query, true);
     frcast.onload = function () {
         const jdata = JSON.parse(frcast.responseText);
-        let tem = jdata[0].Temperature.Value;
-        tem = Math.round((tem - 32) * (5 / 9));
-        const timet = jdata[0].IsDaylight;
-        console.log(timet);
         console.log(jdata);
-        callback(tem,city);
+        callback(city,jdata);
     };
     frcast.send();
 }
 
-function populate(tem,city){
+function populate(city,jdata) {
+    let tem = jdata[0].Temperature.Value;
+    tem = Math.round((tem - 32) * (5 / 9));
+    const timet = jdata[0].IsDaylight;
+    const ic = jdata[0].WeatherIcon;
+    document.querySelector('.icon').innerHTML=`<img src="icons/${ic}.png">`;
+    console.log(timet);
+    if (timet) {
+        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x300/?${city},sunlight`);
+    }
+    else {
+        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x300/?${city},night`);
+    }
     document.querySelector('.card').classList.add('show');
     document.querySelector('.temper').querySelector('span').innerText = tem;
     document.querySelector('.card-details h4').innerText = city;
+
 }
