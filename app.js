624f799efd38c2755ff3e87dc276c6ba0f321eb0
HRITIK DWIVEDI <44141619+hritikrommie@users.noreply.github.com>
2020-10-08 04:20:25 +0530

    conf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const key = 'UaP2ou06sEWBbP49ECYbTP5oGBQy6Xfg';
+const key = 'lJv1VKjEIAG5uHkz68hsktmQeP0i5Abx';
 const form = document.querySelector('.form-control');
 
 form.addEventListener('submit', function (e) {
@@ -44,24 +44,11 @@ function populate(city,jdata,country) {
     const ic = jdata[0].WeatherIcon;
     document.querySelector('.icon').innerHTML=`<img src="icons/${ic}.png">`;
     if (timet) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x220/?${city},sunlight`);
     }
     else {
         document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x220/?${city},night`);
-=======
-        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x200/?${city},sunlight`);
-    }
-    else {
-        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x200/?${city},night`);
->>>>>>> bf1821bb1a3e803afeebc86a4e228393d0b8237f
-=======
-        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x300/?${city},sunlight`);
-    }
-    else {
-        document.querySelector('.card img').setAttribute('src', `https://source.unsplash.com/400x300/?${city},night`);
->>>>>>> parent of 4ff4e2d... changing key
     }
     document.querySelector('.card').classList.add('show');
     document.querySelector('.temper').querySelector('span').innerText = tem;
